652b0664ad6acd546e7e170cb8ed582a8ee9a862
Alina Taranenko <taranenko.alina2004@gmail.com>
2025-03-12 11:33:37 +0200

    Development of the game "Fortune Teller"

---
+++ Lab 3.2 The Fortune Teller Game/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Lab_3._2_The_Fortune_Teller_Game
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ Lab 3.2 The Fortune Teller Game/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace Voroška
+{
+    public partial class MainWindow : Window
+    {
+        private readonly string[] answers = { "Так", "Ні", "Скоріше так", "Скоріше ні" };
+        private static readonly Random rnd = new Random();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AnswerButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(QuestionTextBox.Text))
+            {
+                MessageBox.Show("Будь ласка, введіть питання.", "Попередження", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            int index = rnd.Next(answers.Length);
+            AnswerTextBlock.Text = answers[index];
+        }
+
+        private void QuestionTextBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
